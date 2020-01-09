3809e25f46e2ea6f601f6b1f2ee72842b73488dd
db <darij.b@gmail.com>
2020-01-09 21:11:14 +0100

    Resolve Genericke kolekcije

--- GenerickeKolekcije/GeneričkeKolekcije.cs
+++ GenerickeKolekcije/GeneričkeKolekcije.cs
@@ -9,14 +9,13 @@ namespace Vsite.CSharp.Generici
         public static void TipskiNesigurnaKolekcija()
         {
             ArrayList osobe = new ArrayList();
-            // TODO:000 U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            osobe.Add(new Osoba("Ivica", new DateTime(1991, 1, 1)));
+            osobe.Add(new Osoba("Marica", new DateTime(1991, 1, 1)));
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
 
-            // TODO:001 U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            osobe.Add(2);
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
@@ -25,11 +24,17 @@ namespace Vsite.CSharp.Generici
         public static void GeneričkaKolekcija()
         {
             List<Osoba> osobe = new List<Osoba>();
-            // TODO:002 Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1991, 1, 1)));
+            osobe.Add(new Osoba("Marica", new DateTime(1991, 1, 1)));
 
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
+
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
+            //osobe.Add(2);
         }
 
-        // TODO:003 Pokrenuti i provjeriti testove (2 testa u grupi "TestGeneričkeKolekcije" moraju proći)
 
         static void Main(string[] args)
         {
