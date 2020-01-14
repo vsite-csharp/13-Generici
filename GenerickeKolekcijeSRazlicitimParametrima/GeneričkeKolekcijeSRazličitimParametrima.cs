3b6272468172505d5def20f6680482188abcbf7d
Domagoj Salkić <48859348+domagojsalkic@users.noreply.github.com>
2020-01-14 01:16:41 +0100

    Resolve different parameters collections

--- GenerickeKolekcijeSRazlicitimParametrima/GeneričkeKolekcijeSRazličitimParametrima.cs
+++ GenerickeKolekcijeSRazlicitimParametrima/GeneričkeKolekcijeSRazličitimParametrima.cs
@@ -7,7 +7,6 @@ namespace Vsite.CSharp.Generici
     {
         public static void PridruživanjeDvijuKolekcijaSIstimParametrima()
         {
-            // TODO:020 Pokrenuti program i provjeriti ispise.
 
             Dictionary<string, int> brojevi = new Dictionary<string, int>();
             brojevi.Add("jedan", 1);
@@ -21,11 +20,12 @@ namespace Vsite.CSharp.Generici
 
             Console.WriteLine();
 
-            // TODO:021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            Dictionary<string, int> numbers = brojevi;
 
-
-            // TODO:022 Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            foreach (var e in numbers)
+            {
+                Console.WriteLine($@"""{e.Key}"" = {e.Value}");
+            }
 
         }
 
@@ -36,9 +36,8 @@ namespace Vsite.CSharp.Generici
             numbers.Add("two", "dva");
             numbers.Add("three", "tri");
 
-            // TODO:023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            Dictionary<string, int> brojevi;
 
-            // TODO:024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
             //brojevi = numbers;
 
             foreach (var e in numbers)
@@ -46,8 +45,6 @@ namespace Vsite.CSharp.Generici
                 Console.WriteLine($"\"{e.Key}\" = {e.Value}");
             }
         }
-
-        // TODO:025 Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSRazličitimParametrima" mora proći)
 
         static void Main(string[] args)
         {
