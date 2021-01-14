9900ce1b23de2784d0e24325a5acc2f84e45beae
Tomislav Pesa <topesa@vsite.hr>
2021-01-14 21:04:12 +0100

    Resolve generic restriction

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -8,9 +8,9 @@ namespace Vsite.CSharp.Generici
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO:010 U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
+            // U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
 
-            // TODO:011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
+            // Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
             List<Životinje> živine = new List<Životinje>();
 
             živine.Add(new Sisavac());
@@ -24,7 +24,7 @@ namespace Vsite.CSharp.Generici
             }
         }
 
-        // TODO:012 Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSaČlanovimaRažličitihTipova" mora proći)
+        // Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSaČlanovimaRažličitihTipova" mora proći)
 
         static void Main(string[] args)
         {
