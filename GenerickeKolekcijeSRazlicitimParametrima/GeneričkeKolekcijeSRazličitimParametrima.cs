7fb5943d0e00b351cb94c45420cb58803f1d710a
Mislav <mislav.miseric@gmail.com>
2021-01-14 20:47:50 +0100

    Resolve GeneričkeKolekcijeSRazličitimParametrima

--- GenerickeKolekcijeSRazlicitimParametrima/GeneričkeKolekcijeSRazličitimParametrima.cs
+++ GenerickeKolekcijeSRazlicitimParametrima/GeneričkeKolekcijeSRazličitimParametrima.cs
@@ -21,10 +21,16 @@ namespace Vsite.CSharp.Generici
 
             Console.WriteLine();
 
-            // TODO:021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            // Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            Dictionary<string, int> d = brojevi;
 
+            foreach (var e in brojevi)
+            {
+                Console.WriteLine($@"""{e.Key}"" = {e.Value}");
+            }
 
-            // TODO:022 Prevesti program, pokrenuti ga i provjeriti ispis.
+            Console.WriteLine();
+            // Prevesti program, pokrenuti ga i provjeriti ispis.
 
 
         }
@@ -36,9 +42,10 @@ namespace Vsite.CSharp.Generici
             numbers.Add("two", "dva");
             numbers.Add("three", "tri");
 
-            // TODO:023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            // Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            Dictionary<string, int> brojevi = new Dictionary<string, int>();
 
-            // TODO:024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
+            // Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
             //brojevi = numbers;
 
             foreach (var e in numbers)
@@ -47,7 +54,7 @@ namespace Vsite.CSharp.Generici
             }
         }
 
-        // TODO:025 Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSRazličitimParametrima" mora proći)
+        // Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSRazličitimParametrima" mora proći)
 
         static void Main(string[] args)
         {
