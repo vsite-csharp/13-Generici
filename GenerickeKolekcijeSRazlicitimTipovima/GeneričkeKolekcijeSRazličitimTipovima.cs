fcd2ca1576e4911c4f561f35a9647c86fe1bf1f2
Mislav <mislav.miseric@gmail.com>
2021-01-14 20:40:36 +0100

    Resolve GeneričkeKolekcijeSRazličitimTipovima

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -10,20 +10,22 @@ namespace Vsite.CSharp.Generici
         {
             // U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
 
-            // TODO:011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
-            ArrayList živine = new ArrayList();
-            živine.Add(new Sisavac());
-            živine.Add(new Gmaz());
-            živine.Add(new Riba());
-            živine.Add(new Ptica());
+            // Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
+            List<Zivotinja> živine = new List<Zivotinja>
+            {
+                new Sisavac(),
+                new Gmaz(),
+                new Riba(),
+                new Ptica()
+            };
 
             foreach (var živina in živine)
             {
-                //živina.PokreniSe();
+                živina.PokreniSe();
             }
         }
 
-        // TODO:012 Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSaČlanovimaRažličitihTipova" mora proći)
+        // Pokrenuti i provjeriti testove (test u grupi "TestGeneričkeKolekcijeSaČlanovimaRažličitihTipova" mora proći)
 
         static void Main(string[] args)
         {
