7a0a2c3beb7cee92715b64def1424f4d22e864fe
Anamarija Jozić <anjozic@vsite.hr>
2021-01-15 16:26:38 +0100

    resolve type-safe collections

--- GenerickeKolekcije/GeneričkeKolekcije.cs
+++ GenerickeKolekcije/GeneričkeKolekcije.cs
@@ -9,14 +9,15 @@ namespace Vsite.CSharp.Generici
         public static void TipskiNesigurnaKolekcija()
         {
             ArrayList osobe = new ArrayList();
-            // TODO:000 U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            //  U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1971, 10, 13)));
+            osobe.Add(new Osoba("Marica", new DateTime(1983, 12, 3)));
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
 
-            // TODO:001 U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            //  U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(2);
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
@@ -25,8 +26,13 @@ namespace Vsite.CSharp.Generici
         public static void GeneričkaKolekcija()
         {
             List<Osoba> osobe = new List<Osoba>();
-            // TODO:002 Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            //  Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1971, 10, 13)));
+            osobe.Add(new Osoba("Marica", new DateTime(1983, 12, 3)));
 
+            //osobe.Add(2);
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
         }
 
         // TODO:003 Pokrenuti i provjeriti testove (2 testa u grupi "GeneričkeKolekcije" moraju proći)
