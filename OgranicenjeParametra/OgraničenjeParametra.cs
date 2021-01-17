9ee8b296e71383406df8ea021012e5f9ac0feac9
Vlado Tonković <vlado.tonkovic@outlook.com>
2021-01-17 12:12:31 +0100

    Resolve generic constraints

--- OgranicenjeParametra/OgraničenjeParametra.cs
+++ OgranicenjeParametra/OgraničenjeParametra.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace Vsite.CSharp.Generici
 {
-    // TODO:031 Dodati potrebne promjene da bi se OSobePoVisini mogle uspoređivati po njihovim visinam.
-    class OsobaPoVisini
+    // Dodati potrebne promjene da bi se OSobePoVisini mogle uspoređivati po njihovim visinam.
+    class OsobaPoVisini : IComparable<OsobaPoVisini>
     {
         public OsobaPoVisini(string ime, int visinaCm)
         {
@@ -22,14 +22,21 @@ namespace Vsite.CSharp.Generici
             return $"{Ime} ({Visina} cm)";
         }
 
+        public int CompareTo(OsobaPoVisini other)
+        {
+            return Visina - other.Visina;
+        }
     }
 
     class OgraničenjeParametra
     {
-        // TODO:030 Definirati metodu Manji tako da vraća manji od dva proslijeđena argumenta pozivom njihove metode CompareTo iz sučelja IComparable<T>.
-        public static T Manji<T>(T prvi, T drugi)
+        // Definirati metodu Manji tako da vraća manji od dva proslijeđena argumenta pozivom njihove metode CompareTo iz sučelja IComparable<T>.
+        public static T Manji<T>(T prvi, T drugi) where T : IComparable<T>
         {
-            throw new NotImplementedException();
+            if (prvi.CompareTo(drugi) < 0)
+                return prvi;
+            
+            return drugi;
         }
 
         private static void IspišiManjiBroj(double prvi, double drugi)
@@ -47,9 +54,9 @@ namespace Vsite.CSharp.Generici
             Console.WriteLine($"Između '{prva}' i '{druga}', manji je '{Manji(prva, druga)}'");
         }
 
-        // TODO:031 Pokrenuti program i provjeriti ispis.
+        // Pokrenuti program i provjeriti ispis.
 
-        // TODO:032 Pokrenuti testove (3 testa u grupi "TestOgraničenjaParametra" moraju proći)
+        // Pokrenuti testove (3 testa u grupi "TestOgraničenjaParametra" moraju proći)
 
         static void Main(string[] args)
         {
