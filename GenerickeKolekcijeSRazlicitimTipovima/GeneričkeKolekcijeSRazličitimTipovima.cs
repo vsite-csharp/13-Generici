3648a22b11de9e2a8d54054c908b407c4873b728
Ivan Spanja <IvanSp@dspace.hr>
2022-01-13 20:31:16 +0100

    Resolve Generičke kolkcije s različitim parametrima

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -8,10 +8,10 @@ namespace Vsite.CSharp.Generici
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO:010 U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
+            // 010 U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
 
-            // TODO:011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
-            ArrayList živine = new ArrayList();
+            // 011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
+            var živine = new List<Životinja>();
             živine.Add(new Sisavac());
             živine.Add(new Gmaz());
             živine.Add(new Riba());
@@ -19,7 +19,7 @@ namespace Vsite.CSharp.Generici
 
             foreach (var živina in živine)
             {
-                //živina.PokreniSe();
+                živina.PokreniSe();
             }
         }
 
