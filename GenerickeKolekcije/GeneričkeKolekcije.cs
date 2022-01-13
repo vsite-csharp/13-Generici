970af7627a51e06474f086a4b57923532d8c713f
jandela <andjela.cirjak@gmail.com>
2022-01-13 20:21:32 +0100

    Resolve non-generic vs generic collections

--- GenerickeKolekcije/GeneričkeKolekcije.cs
+++ GenerickeKolekcije/GeneričkeKolekcije.cs
@@ -9,14 +9,15 @@ namespace Vsite.CSharp.Generici
         public static void TipskiNesigurnaKolekcija()
         {
             ArrayList osobe = new ArrayList();
-            // TODO:000 U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            // U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1980, 2, 29)));
+            osobe.Add(new Osoba("Marica", new DateTime(1993, 1, 1)));
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
 
-            // TODO:001 U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            // U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(23);
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
@@ -25,11 +26,16 @@ namespace Vsite.CSharp.Generici
         public static void GeneričkaKolekcija()
         {
             List<Osoba> osobe = new List<Osoba>();
-            // TODO:002 Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            // Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1980, 2, 29)));
+            osobe.Add(new Osoba("Marica", new DateTime(1993, 1, 1)));
+            //osobe.Add(23); //ne može!
+            foreach (Osoba osoba in osobe)
+                Console.WriteLine(osoba.Ime);
 
         }
 
-        // TODO:003 Pokrenuti i provjeriti testove (2 testa u grupi "GeneričkeKolekcije" moraju proći)
+        // Pokrenuti i provjeriti testove (2 testa u grupi "GeneričkeKolekcije" moraju proći)
 
         static void Main(string[] args)
         {
