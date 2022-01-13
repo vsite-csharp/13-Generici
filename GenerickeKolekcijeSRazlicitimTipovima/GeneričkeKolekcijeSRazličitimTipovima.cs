97933e5dd1f244abdf3bb796d06f64c05085525c
Ana Lovric <alovric@dspace.hr>
2022-01-13 20:29:48 +0100

    Resloved generic collections for different types.

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -8,10 +8,11 @@ namespace Vsite.CSharp.Generici
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO:010 U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
+            // 010 U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
 
-            // TODO:011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
-            ArrayList živine = new ArrayList();
+            // 011 Klasu ArrayList nadomjestiti generičkom listom List<Životinja>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
+            //ArrayList živine = new ArrayList();
+            var živine = new List<Životinja>();
             živine.Add(new Sisavac());
             živine.Add(new Gmaz());
             živine.Add(new Riba());
@@ -19,11 +20,11 @@ namespace Vsite.CSharp.Generici
 
             foreach (var živina in živine)
             {
-                //živina.PokreniSe();
+                živina.PokreniSe();
             }
         }
 
-        // TODO:012 Pokrenuti i provjeriti testove (test u grupi "GeneričkeKolekcijeSaČlanovimaRazličitihTipova" mora proći)
+        // 012 Pokrenuti i provjeriti testove (test u grupi "GeneričkeKolekcijeSaČlanovimaRazličitihTipova" mora proći)
 
         static void Main(string[] args)
         {
