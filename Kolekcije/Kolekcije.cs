1a3893ded028ab80e62ec152e236b73e856f113e
Robert Tušek <rotusek@vsite.hr>
2023-01-12 20:59:06 +0100

    Resolve generic collections

--- Kolekcije/Kolekcije.cs
+++ Kolekcije/Kolekcije.cs
@@ -10,7 +10,8 @@ namespace Vsite.CSharp.Generici
         {
             ArrayList osobe = new ArrayList();
             // TODO:000 U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            osobe.Add(new Osoba("Ivica", new DateTime(1463, 4, 23)));
+            osobe.Add(new Osoba("Marica", new DateTime(1464, 5, 23)));
 
             foreach (Osoba osoba in osobe)
             {
@@ -18,7 +19,7 @@ namespace Vsite.CSharp.Generici
             }
 
             // TODO:001 U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            osobe.Add(44);
 
             foreach (Osoba osoba in osobe)
             {
@@ -30,7 +31,21 @@ namespace Vsite.CSharp.Generici
         {
             List<Osoba> osobe = new List<Osoba>();
             // TODO:002 Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1463, 4, 23)));
+            osobe.Add(new Osoba("Marica", new DateTime(1464, 5, 23)));
 
+            foreach (Osoba osoba in osobe)
+            {
+                Console.WriteLine(osoba.Ime);
+            }
+
+            
+            //osobe.Add(44);
+
+            foreach (Osoba osoba in osobe)
+            {
+                Console.WriteLine(osoba.Ime);
+            }
         }
 
         // TODO:003 Pokrenuti i provjeriti testove (2 testa u grupi "Kolekcije" moraju proći)
