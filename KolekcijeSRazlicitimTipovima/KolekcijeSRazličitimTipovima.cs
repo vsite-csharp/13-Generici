74ec3f626d0755cece596eb0161f66789e26aa17
ipandzicd <ivan.pandzic@gmail.com>
2023-01-12 21:11:04 +0100

    Resolve generic collections with different parameters

--- KolekcijeSRazlicitimTipovima/KolekcijeSRazličitimTipovima.cs
+++ KolekcijeSRazlicitimTipovima/KolekcijeSRazličitimTipovima.cs
@@ -8,17 +8,17 @@ namespace Vsite.CSharp.Generici
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO:010 U datoteci Životinja.cs definirati sučelje (interface) IŽivotinja s metodom void PokreniSe().
-            // TODO:011 U deklariracijama klasa Gmaz, Ptica, Riba i Sisavac navesti da one implementiraju sučelje IŽivotinja.
+            // :010 U datoteci Životinja.cs definirati sučelje (interface) IŽivotinja s metodom void PokreniSe().
+            // :011 U deklariracijama klasa Gmaz, Ptica, Riba i Sisavac navesti da one implementiraju sučelje IŽivotinja.
 
-            // TODO:012 Klasu ArrayList nadomjestiti generičkom listom List<IŽivotinja>.
+            // :012 Klasu ArrayList nadomjestiti generičkom listom List<IŽivotinja>.
             var živine = new List<IŽivotinja>();
             živine.Add(new Sisavac());
             živine.Add(new Gmaz());
             živine.Add(new Riba());
             živine.Add(new Ptica());
 
-            // TODO:013 Otkomentirati naredbu u petlji, prevesti program, provjeriti da nema pogrešaka te pokrenuti program.
+            // :013 Otkomentirati naredbu u petlji, prevesti program, provjeriti da nema pogrešaka te pokrenuti program.
             foreach (var živina in živine)
             {
                 živina.PokreniSe();
