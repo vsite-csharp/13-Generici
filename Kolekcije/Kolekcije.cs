791304a00fb96660930f62f17bf7864f023bfdb3
Fran Štambuk <fran.stambuk@gmail.com>
2023-01-12 21:05:04 +0100

    Resolved collection and collection with various types

--- Kolekcije/Kolekcije.cs
+++ Kolekcije/Kolekcije.cs
@@ -9,16 +9,17 @@ namespace Vsite.CSharp.Generici
         public static void TipskiNesigurnaKolekcija()
         {
             ArrayList osobe = new ArrayList();
-            // TODO:000 U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            // U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1463, 4, 20)));
+            osobe.Add(new Osoba("Marica", new DateTime(1460, 2, 20)));
 
             foreach (Osoba osoba in osobe)
             {
                 Console.WriteLine(osoba.Ime);
             }
 
-            // TODO:001 U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+            // U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(44);
 
             foreach (Osoba osoba in osobe)
             {
@@ -30,7 +31,21 @@ namespace Vsite.CSharp.Generici
         {
             List<Osoba> osobe = new List<Osoba>();
             // TODO:002 Ponoviti dodavanja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+            osobe.Add(new Osoba("Ivica", new DateTime(1463, 4, 20)));
+            osobe.Add(new Osoba("Marica", new DateTime(1460, 2, 20)));
 
+            foreach (Osoba osoba in osobe)
+            {
+                Console.WriteLine(osoba.Ime);
+            }
+
+            // U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+            //osobe.Add(44);
+
+            foreach (Osoba osoba in osobe)
+            {
+                Console.WriteLine(osoba.Ime);
+            }
         }
 
         // TODO:003 Pokrenuti i provjeriti testove (2 testa u grupi "Kolekcije" moraju proći)
