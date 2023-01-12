d03637fe2eaf0fa0d1dad141806292b010e378c4
Ivor Novosel <ivnovosel@vsite.hr>
2023-01-12 21:11:14 +0100

    Resolve generic collections with different parameters

--- KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
+++ KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
@@ -7,7 +7,7 @@ namespace Vsite.CSharp.Generici
     {
         public static void PridruživanjeDvijuKolekcijaSIstimParametrima()
         {
-            // TODO:020 Pokrenuti program i provjeriti ispise.
+            // 020 Pokrenuti program i provjeriti ispise.
 
             Dictionary<string, int> brojevi = new Dictionary<string, int>();
             brojevi.Add("jedan", 1);
@@ -21,9 +21,17 @@ namespace Vsite.CSharp.Generici
 
             Console.WriteLine();
 
-            // TODO:021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            // 021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            Dictionary<string, int> novi = new Dictionary<string, int>(brojevi);
 
-            // TODO:022 Prevesti program, pokrenuti ga i provjeriti ispis.
+            foreach (var e in novi)
+            {
+                Console.WriteLine($@"""{e.Key}"" = {e.Value}");
+            }
+
+            Console.WriteLine();
+
+            // 022 Prevesti program, pokrenuti ga i provjeriti ispis.
 
         }
 
@@ -34,9 +42,10 @@ namespace Vsite.CSharp.Generici
             numbers.Add("two", "dva");
             numbers.Add("three", "tri");
 
-            // TODO:023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            // 023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            var brojevi = new Dictionary<string, int>();
 
-            // TODO:024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
+            // 024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
             //brojevi = numbers;
 
             foreach (var e in numbers)
@@ -45,7 +54,7 @@ namespace Vsite.CSharp.Generici
             }
         }
 
-        // TODO:025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
+        // 025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
 
         static void Main(string[] args)
         {
