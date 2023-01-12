b2e665a0359994e26e6b5a43b3a0dd0f8fb34a38
Ivor Novosel <ivnovosel@vsite.hr>
2023-01-12 21:04:55 +0100

    Resolve collection with various types

--- KolekcijeSRazlicitimTipovima/KolekcijeSRazličitimTipovima.cs
+++ KolekcijeSRazlicitimTipovima/KolekcijeSRazličitimTipovima.cs
@@ -8,24 +8,24 @@ namespace Vsite.CSharp.Generici
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO:010 U datoteci Životinja.cs definirati sučelje (interface) IŽivotinja s metodom void PokreniSe().
-            // TODO:011 U deklariracijama klasa Gmaz, Ptica, Riba i Sisavac navesti da one implementiraju sučelje IŽivotinja.
+            // 010 U datoteci Životinja.cs definirati sučelje (interface) IŽivotinja s metodom void PokreniSe().
+            // 011 U deklariracijama klasa Gmaz, Ptica, Riba i Sisavac navesti da one implementiraju sučelje IŽivotinja.
 
-            // TODO:012 Klasu ArrayList nadomjestiti generičkom listom List<IŽivotinja>.
-            var živine = new ArrayList();
+            // 012 Klasu ArrayList nadomjestiti generičkom listom List<IŽivotinja>.
+            var živine = new List<IŽivotinja>();
             živine.Add(new Sisavac());
             živine.Add(new Gmaz());
             živine.Add(new Riba());
             živine.Add(new Ptica());
 
-            // TODO:013 Otkomentirati naredbu u petlji, prevesti program, provjeriti da nema pogrešaka te pokrenuti program.
+            // 013 Otkomentirati naredbu u petlji, prevesti program, provjeriti da nema pogrešaka te pokrenuti program.
             foreach (var živina in živine)
             {
-                //živina.PokreniSe();
+                živina.PokreniSe();
             }
         }
 
-        // TODO:014 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSaČlanovimaRazličitihTipova" mora proći)
+        // 014 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSaČlanovimaRazličitihTipova" mora proći)
 
         static void Main(string[] args)
         {
