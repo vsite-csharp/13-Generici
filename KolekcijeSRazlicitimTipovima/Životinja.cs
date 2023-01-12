25e966458e9c5542a08ca9e3d4f3aa1b6a3b9e8a
Luka <Luka@DESKTOP-PUVMF0P>
2023-01-12 21:05:04 +0100

    Resoslve collection with various types

--- KolekcijeSRazlicitimTipovima/Životinja.cs
+++ KolekcijeSRazlicitimTipovima/Životinja.cs
@@ -1,6 +1,11 @@
 ﻿namespace Vsite.CSharp.Generici
 {
-    class Sisavac
+
+    interface IŽivotinja
+    {
+        void PokreniSe();
+    }
+    class Sisavac : IŽivotinja
     {
         public void PokreniSe()
         {
@@ -8,7 +13,7 @@
         }
     }
 
-    class Gmaz
+    class Gmaz : IŽivotinja
     {
         public void PokreniSe()
         {
@@ -16,7 +21,7 @@
         }
     }
 
-    class Riba
+    class Riba : IŽivotinja
     {
         public void PokreniSe()
         {
@@ -24,7 +29,7 @@
         }
     }
 
-    class Ptica
+    class Ptica : IŽivotinja
     {
         public void PokreniSe()
         {
