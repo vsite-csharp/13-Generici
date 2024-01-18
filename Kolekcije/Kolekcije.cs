5b6c6feb097c698403b6420756a60ac1ff7bc9a8
Robert Tušek <rotusek@vsite.hr>
2024-01-18 20:51:57 +0100

    Implement generic list with class Osoba

--- Kolekcije/Kolekcije.cs
+++ Kolekcije/Kolekcije.cs
@@ -7,14 +7,19 @@
         // TODO:001 U metodi DodajJelo implementirati dodavanje osobe u listu.
         public void DodajJelo(Osoba osoba)
         {
-
+            osobe.Add(osoba);
         }
 
         // TODO:002 U metodi IspišiDnevniMenu implementirati kod koji će ispisati imena svih osoba.
         public void IspišiDnevniMenu()
         {
-
+            foreach(var o in osobe)
+            {
+                Console.WriteLine(o.Ime);
+            }
         }
+        private readonly List<Osoba> osobe = new List<Osoba>();
+
     }
 
     internal static class GeneričkeKolekcije
@@ -26,8 +31,9 @@
 
             var jsv = new JelovnikStareVještice();
 
-            // TODO:003 dodati pozive metode DodajJelo za dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja.
-
+            // TODO:003 dodati pozive metode DodajJelo za8 dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja.
+            jsv.DodajJelo(new Osoba("Ivica", new DateTime(1945, 12, 3)));
+            jsv.DodajJelo(new Osoba("Marica", new DateTime(1955, 12, 5)));
             // TODO:004 Prevesti program, pokrenuti ga i provjeriti ispis.
             // TODO:005 Pokrenuti i provjeriti testove (Oba testa u grupi "Kolekcije" moraju proći)
             jsv.IspišiDnevniMenu();
