22059e4ce4f55980eb8a3a52a109bc3ae1392c9d
Mislav Ćavarušić <micavaru@vsite.hr>
2024-01-18 20:51:50 +0100

    Implement generic list with class Osoba

--- Kolekcije/Kolekcije.cs
+++ Kolekcije/Kolekcije.cs
@@ -2,19 +2,24 @@
 {
     internal class JelovnikStareVještice
     {
-        // TODO:000 U klasu dodati kao član generičku klasu List<T> s elementima tipa Osoba.
+        // :000 U klasu dodati kao član generičku klasu List<T> s elementima tipa Osoba.
 
-        // TODO:001 U metodi DodajJelo implementirati dodavanje osobe u listu.
+        // :001 U metodi DodajJelo implementirati dodavanje osobe u listu.
         public void DodajJelo(Osoba osoba)
         {
-
+            osobe.Add(osoba);
         }
 
-        // TODO:002 U metodi IspišiDnevniMenu implementirati kod koji će ispisati imena svih osoba.
+        // :002 U metodi IspišiDnevniMenu implementirati kod koji će ispisati imena svih osoba.
         public void IspišiDnevniMenu()
         {
-
+            foreach(var o in osobe) 
+            {
+                Console.WriteLine(o.Ime);
+            }
         }
+
+        private readonly List<Osoba> osobe = new List<Osoba>();
     }
 
     internal static class GeneričkeKolekcije
@@ -26,10 +31,11 @@
 
             var jsv = new JelovnikStareVještice();
 
-            // TODO:003 dodati pozive metode DodajJelo za dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja.
-
-            // TODO:004 Prevesti program, pokrenuti ga i provjeriti ispis.
-            // TODO:005 Pokrenuti i provjeriti testove (Oba testa u grupi "Kolekcije" moraju proći)
+            // :003 dodati pozive metode DodajJelo za dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja.
+            jsv.DodajJelo(new Osoba("Ivica", new DateTime(1957, 12, 5)));
+            jsv.DodajJelo(new Osoba("Marica", new DateTime(1977, 11, 5)));
+            // :004 Prevesti program, pokrenuti ga i provjeriti ispis.
+            // :005 Pokrenuti i provjeriti testove (Oba testa u grupi "Kolekcije" moraju proći)
             jsv.IspišiDnevniMenu();
 
             Console.WriteLine("\nGOTOVO!!!");
