417fb993e558d5006c3a3aaff268a69e2b4a1be1
Marko Pavić <mapavic@vsite.hr>
2024-01-18 21:04:00 +0100

    Cannot assign identical generic collections with different parameters

--- KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
+++ KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
@@ -4,7 +4,7 @@
     {
         public static void PridruživanjeDvijuKolekcijaSIstimParametrima()
         {
-            // TODO:020 Pokrenuti program i provjeriti ispise.
+            // 020 Pokrenuti program i provjeriti ispise.
 
             Dictionary<string, int> brojevi = new Dictionary<string, int>();
             brojevi.Add("jedan", 1);
@@ -18,9 +18,9 @@
 
             Console.WriteLine();
 
-            // TODO:021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            // 021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
 
-            // TODO:022 Prevesti program, pokrenuti ga i provjeriti ispis.
+            // 022 Prevesti program, pokrenuti ga i provjeriti ispis.
 
         }
 
@@ -31,18 +31,21 @@
             numbers.Add("two", "dva");
             numbers.Add("three", "tri");
 
-            // TODO:023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+			// 023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+			Dictionary<string, int> brojevi = new Dictionary<string, int>();
+			brojevi.Add("jedan", 1);
+			brojevi.Add("dva", 2);
+			brojevi.Add("tri", 3);
+			// 024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
+			//brojevi = numbers;
 
-            // TODO:024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
-            //brojevi = numbers;
-
-            foreach (var e in numbers)
+			foreach (var e in numbers)
             {
                 Console.WriteLine($"\"{e.Key}\" = {e.Value}");
             }
         }
 
-        // TODO:025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
+        // 025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
 
         static void Main()
         {
