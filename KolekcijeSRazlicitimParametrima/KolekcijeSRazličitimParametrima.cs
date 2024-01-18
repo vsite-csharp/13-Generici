1d8fed61f0f70a16c0d1c68ad722d4dcb416225b
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-01-18 21:17:24 +0100

    - test Collections with different parameters; - test Limitations of Parameters

--- KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
+++ KolekcijeSRazlicitimParametrima/KolekcijeSRazličitimParametrima.cs
@@ -4,7 +4,7 @@
     {
         public static void PridruživanjeDvijuKolekcijaSIstimParametrima()
         {
-            // TODO:020 Pokrenuti program i provjeriti ispise.
+            // :020 Pokrenuti program i provjeriti ispise.
 
             Dictionary<string, int> brojevi = new Dictionary<string, int>();
             brojevi.Add("jedan", 1);
@@ -18,9 +18,13 @@
 
             Console.WriteLine();
 
-            // TODO:021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
-
-            // TODO:022 Prevesti program, pokrenuti ga i provjeriti ispis.
+            // :021 Inicijalizirati novi rječnik tipa Dictionary<string, int>, pridružiti mu sadržaj rječnika 'brojevi' te ponoviti ispis svih članova novog rječnika. 
+            Dictionary<string, int> broj = brojevi;
+            foreach (var e in broj)
+            {
+                Console.WriteLine($@"""{e.Key}"" = {e.Value}");
+            }
+            // :022 Prevesti program, pokrenuti ga i provjeriti ispis.
 
         }
 
@@ -31,9 +35,12 @@
             numbers.Add("two", "dva");
             numbers.Add("three", "tri");
 
-            // TODO:023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
-
-            // TODO:024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
+            // :023 Inicijalizirati novi rječnik 'brojevi' tipa Dictionary<string, int>
+            Dictionary<string, int> brojevi = new Dictionary<string, int>();
+            brojevi.Add("jedan", 1);
+            brojevi.Add("dva", 2);
+            brojevi.Add("tri", 3);
+            // :024 Otkomentirati donju naredbu i provjerite pogrešku koju prevoditelj prijavljuje.
             //brojevi = numbers;
 
             foreach (var e in numbers)
@@ -42,7 +49,7 @@
             }
         }
 
-        // TODO:025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
+        // :025 Pokrenuti i provjeriti testove (test u grupi "KolekcijeSRazličitimParametrima" mora proći)
 
         static void Main()
         {
